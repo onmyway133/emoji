be1bcf77415ad34199ed4a3de3480a80964abbe3
Khoa Pham <onmyway133@gmail.com>
2016-06-16 00:09:14 +0200

    Add skeleton

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-# emoji
-:smile:
-
-
-## License
-
-**emoji** is available under the MIT license. See the [LICENSE](https://github.com/onmyway133/emoji/blob/master/LICENSE.md) file for more info.
+<table><colgroup><col><col><col></colgroup><tr>emoji</tr><tr>alias</tr><tr>name</tr></table>
--- script.swift
+++ script.swift
@@ -5,4 +5,54 @@
 import Foundation
 import Smile
 
-print(Smile.emoji(alias: "japanese_castle"))
+// MARK: - Extension
+
+extension Int {
+  func times(@noescape block: () -> Void) {
+    for _ in 0..<self {
+      block()
+    }
+  }
+}
+
+// MARK: - Helper
+
+func add(string: String) {
+  result += string
+}
+
+func tag(name: String, @noescape block: () -> Void) {
+  add("<\(name)>")
+  block()
+  add("</\(name)>")
+}
+
+func tag(name: String, content: String) {
+  add("<\(name)>")
+  add(content)
+  add("</\(name)>")
+}
+
+// MARK: - Main
+
+var result = ""
+
+tag("table") {
+  tag("colgroup") {
+    3.times {
+      add("<col>")
+    }
+  }
+
+  tag("tr", content: "emoji")
+  tag("tr", content: "alias")
+  tag("tr", content: "name")
+}
+
+// MARK: - Write
+
+do {
+  try result.writeToFile("README.md", atomically: true, encoding: NSUTF8StringEncoding)
+} catch {
+  print("something goes wrong")
+}
